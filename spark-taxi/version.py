2ef37c60717b11225508f658ad3f21baf773c6d3
Juan Manuel Bogarin <43458719+jmbogarin@users.noreply.github.com>
2026-03-07 19:47:31 +0000

    Added Spark analysis

---
+++ spark-taxi/version.py
@@ -0,0 +1,11 @@
+import pyspark
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .master("local[*]") \
+    .appName('test') \
+    .getOrCreate()
+
+print(f"Spark version: {spark.version}")
+
+spark.stop()
